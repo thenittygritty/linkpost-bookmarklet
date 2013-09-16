c5bf343e0fb6c9d723e7ffba28270215cc167b28
Kahlil Lechelt <hello@kahlil.co>
2013-09-16 10:57:52 +0200

    Add basic Gruntfile and min task.

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= props.license %> */\n',
+    // The path to the bookmarklet files.
+    bmpath: 'assets/js/bookmarklet',
+    // Task configuration.
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      bookmarklet: {
+        src: '<%= bmpath %>/bookmarklet.js',
+        dest: '<%= bmpath %>/bookmarklet.min.js'
+      },
+      linkpost: {
+        src: '<%= bmpath %>/linkpost.js',
+        dest: '<%= bmpath %>/linkpost.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+				src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+          files: '<%= jshint.gruntfile.src %>',
+          tasks: ['jshint:gruntfile']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
+
+};
